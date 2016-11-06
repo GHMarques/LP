058b42d45fd06a25201999aa7d26ef2c02c360c0
Wendell Ferreira dos Santos <wendellf.sa@gmail.com>
2016-11-06 20:44:17 -0200

    Add files via upload

--- TP3/binario.hs
+++ TP3/binario.hs
@@ -1,7 +1,20 @@
-binario :: Int->[Int]
-x = []
-binario n = 
-			if ((n > 0) && (n `mod` 2 == 1))
-			then x <- 1:[] 
-			else  x <- 0:[]
-			binario (n-1)
+{-
+binario 20 -> 10  [0]
+binario 10 -> 5   [0,0]
+binario 5  -> 2   [1,0,0]
+binario 2  -> 1   [0,1,0,0]
+binario 1  -> 0   [1,0,1,0,0]
+-}
+
+magic :: Integer -> [Integer] -> [Integer]
+magic a all@(_)
+  | a == 0 = all
+  | otherwise = (magic (truncate ( fromIntegral a / fromIntegral 2)) ([ceiling ( fromIntegral(a `mod` 2) )] ++ all))
+
+
+binario :: Integer -> [Integer]
+binario b
+  | b == 0 = [0]
+  | otherwise = magic b []
+
+
