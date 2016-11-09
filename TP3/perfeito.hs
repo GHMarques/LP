d00c61aa0d5dc454a70e817ac0157ffd12aee5f2
Gustavo Marques <GHMarques@users.noreply.github.com>
2016-11-09 15:29:47 -0200

    Add files via upload

--- TP3/perfeito.hs
+++ TP3/perfeito.hs
@@ -4,10 +4,9 @@ obter_divisores n = [ x | x <- [1 .. n-1], ((n `mod` x) == 0)]
 somatorio :: [Int]->Int
 somatorio [] = 0
 somatorio (x:xs) =
-	x + somatorio xs
+ x + somatorio xs
 
 perfeito :: Int->Bool
-perfeito n = 
-				if(somatorio (obter_divisores n)) == n
-				then True
-				else False
+perfeito n
+ | somatorio (obter_divisores n) == n  = True
+ | otherwise  = False
